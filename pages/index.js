8d0ed927f2d08123c12cf89a3c8b211a53e3f463
jujiuwork <jujiuwork@daxiao>
2022-08-23 16:41:01 +0800

    home

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,45 @@ import { withProjectPage } from '../components';
 
 import Router from 'next/router';
 import { useState, useCallback, useEffect, useRef } from 'react';
-import { Button, Box } from 'grommet';
+import { Button, Box, Text, Heading } from 'grommet';
 import { Close } from 'grommet-icons';
 
+const titleStyle = {
+  fontSize: '5vw',
+  lineHeight: '5vw',
+  color: '#fff',
+};
+
 const Component = () => {
-  return <Box>home</Box>;
+  return (
+    <Box height={'100%'} justify='center'>
+      <Box
+        margin={{
+          left: '5vw',
+          alginSelf: 'center',
+        }}
+        width={'31vw'}
+        height={'20vw'}
+      >
+        <Box flex={true} style={titleStyle}>
+          上海大晓智能
+        </Box>
+        <Box flex={true} style={titleStyle}>
+          科技有限公司
+        </Box>
+        <Box margin={'1.5vw 0'} background={'#fff'} height='1px' width={'auto'}></Box>
+        <Box
+          style={{
+            fontSize: '1.5vw',
+            lineHeight: '1.5vw',
+            color: '#fff',
+          }}
+        >
+          科技服务美好生活
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 const Page = withProjectPage({ Component, background: 'linear-gradient(to bottom right, #00CCFF, #0033CC)' });
